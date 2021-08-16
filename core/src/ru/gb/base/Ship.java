57615859b02bcd682f1f9c2f258ad2824d5a6246
KorEugene <johnkorch@yandex.ru>
2021-08-16 20:16:45 +0300

    hw6 ready

--- core/src/ru/gb/base/Ship.java
+++ core/src/ru/gb/base/Ship.java
@@ -8,7 +8,9 @@ import ru.gb.math.Rect;
 import ru.gb.pool.BulletPool;
 import ru.gb.sprite.Bullet;
 
-public class Ship extends Sprite {
+public abstract class Ship extends Sprite {
+
+    private static final float DAMAGE_ANIMATE_INTERVAL = 0.1f;
 
     protected final Vector2 v0;
     protected final Vector2 v;
@@ -25,6 +27,8 @@ public class Ship extends Sprite {
 
     protected float reloadInterval;
     protected float reloadTimer;
+
+    private float damageAnimateTimer = DAMAGE_ANIMATE_INTERVAL;
 
     public Ship() {
         v0 = new Vector2();
@@ -50,7 +54,27 @@ public class Ship extends Sprite {
             reloadTimer = 0f;
             shoot();
         }
+        damageAnimateTimer += delta;
+        if (damageAnimateTimer >= DAMAGE_ANIMATE_INTERVAL) {
+            frame = 0;
+        }
     }
+
+    public void damage(int damage) {
+        hp -= damage;
+        if (hp <= 0) {
+            hp = 0;
+            destroy();
+        }
+        frame = 1;
+        damageAnimateTimer = 0f;
+    }
+
+    public int getBulletDamage() {
+        return bulletDamage;
+    }
+
+    public abstract boolean isBulletCollision(Bullet bullet);
 
     private void shoot() {
         Bullet bullet = bulletPool.obtain();
